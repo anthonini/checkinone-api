e4ab652f216e5abe30a2e4acbb37a7aa5348bd9e
Anthonini <anthonini@gmail.com>
2025-05-23 18:14:23 -0300

    Commit inicial do projeto.

---
+++ src/main/java/com/checkinone/CheckinoneApplication.java
@@ -0,0 +1,13 @@
+package com.checkinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkinone/CheckinoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
