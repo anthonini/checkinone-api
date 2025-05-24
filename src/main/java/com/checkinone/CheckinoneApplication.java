fc86d8a6a8add1e755236789574c54238c8d09cd
Anthonini <anthonini@gmail.com>
2025-05-24 16:15:03 -0300

    Desativado o uso automático de transações.

--- src/main/java/com/checkinone/CheckinoneApplication.java
+++ src/main/java/com/checkinone/CheckinoneApplication.java
@@ -2,8 +2,10 @@ package com.checkinone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class CheckinoneApplication {
 
 	public static void main(String[] args) {
