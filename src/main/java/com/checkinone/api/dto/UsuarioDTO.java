138f7a5fcc14051533c595809c84a5dbe693379d
Anthonini <anthonini@gmail.com>
2025-05-24 17:21:22 -0300

    Incluído o campo id no DTO de usuário

--- src/main/java/com/checkinone/api/dto/UsuarioDTO.java
+++ src/main/java/com/checkinone/api/dto/UsuarioDTO.java
@@ -3,10 +3,15 @@ package com.checkinone.api.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UsuarioDTO {
+	
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	private Long id;
 
 	@Email(message = "E-mail inválido")
 	@NotBlank(message = "E-mail é obrigatório")
@@ -16,6 +21,14 @@ public class UsuarioDTO {
 	private String senha;
 	
 	private List<PermissaoDTO> permissoes = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getEmail() {
 		return email;
